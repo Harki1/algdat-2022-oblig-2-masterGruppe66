031aedc7f05979b997207c01edc613e13a1217e1
HassanHussain10 <73237055+HassanHussain10@users.noreply.github.com>
2022-10-04 21:10:22 +0200

    andre prototype av oppgave 4 commit

--- src/main/java/no/oslomet/cs/algdat/Oblig2/DobbeltLenketListe.java
+++ src/main/java/no/oslomet/cs/algdat/Oblig2/DobbeltLenketListe.java
@@ -9,7 +9,7 @@ import java.util.ConcurrentModificationException;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
-import java.util*;
+import java.util.*;
 public class DobbeltLenketListe<T> implements Liste<T> {
 
     /**
